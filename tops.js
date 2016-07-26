66c2996c9cf59b06776f04aaa1a1d1407c8fe320
Robin Millette <robin@millette.info>
2016-07-26 15:18:41 -0400

    Top events.

---
+++ tops.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+'use strict'
+
+// npm
+const _ = require('lodash')
+const db = require('nano')('http://localhost:5984/evs2')
+
+// 'http://localhost:5984/evs2/_design/app/_view/reposhortname?group_level=2'
+db.view('app', 'repofullname', { group_level: 2 }, (err, bod) => {
+  const z = _.groupBy(bod.rows, (x) => x.key[0])
+  const more = {}
+  let r
+  console.log(bod.rows.length)
+  console.log(Object.keys(z))
+  for (r in z) {
+    more[r] = _.sortBy(z[r], 'value').reverse().slice(0, 100)
+      .map((x) => {
+        return {
+          name: x.key[1],
+          value: x.value
+        }
+      })
+  }
+  console.log(JSON.stringify(more, null, ' '))
+})
+
