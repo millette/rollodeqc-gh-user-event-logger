683f44f1494f5895566c9d8cf48e87e34ad74d21
Robin Millette <robin@millette.info>
2016-07-27 03:48:32 -0400

    Improved output.

--- events.js
+++ events.js
@@ -8,40 +8,29 @@ const _ = require('lodash')
 const db = require('nano')('http://localhost:5984/evs2')
 const rlp = require('rate-limit-promise')
 
-// const contributors = require('./contributors.json').rows.map((x) => x.id.split(':')[1])
-
-const request = rlp(3, 10000)
-
 db.list({include_docs: true, startkey: 'github:user:id:', endkey: 'github:user:id:\ufff0'}, (err, body) => {
+  const delay = 2000
+  const request = rlp(5, delay) // 5, 7500
+  let count = body.rows.length
   const store = { }
   const store2 = { }
   const contributors = []
+  const timer = setInterval(() => {
+    console.log(new Date(), 'ping', count, 'to go')
+    if (count < 1) { clearInterval(timer) }
+  }, 3 * delay)
   if (err) { return console.log('err:', err) }
-  setInterval(() => console.log(new Date(), 'ping'), 5000)
   console.log(body.rows.length)
-  // console.log(body.rows[0])
-  body.rows.forEach((doc) => {
+  _.shuffle(body.rows).forEach((doc) => {
     contributors.push(doc.doc.login)
     store[doc.doc.login] = doc.doc
     store2[doc.doc.login] = doc.doc
   })
 
-  // console.log(contributors.slice(0, 1))
-  // if (false)
   contributors.forEach((user) => {
-    console.log('USER:', user)
     request()
       .then(() => ghUser(user, store))
-      .then((doc) => {
-        // console.log('STORED:', store[doc.login])
-        // console.log('STORED2:', store2[doc.login])
-        Object.assign(store2[doc.login], doc)
-        // console.log('OUT:', out)
-        // console.log('STORED2:', store2[doc.login])
-        // doc._id = store[doc.login]._id
-        // doc._rev = store[doc.login]._rev
-        return store2[doc.login] // out
-      })
+      .then((doc) => Object.assign(store2[doc.login], doc))
       .then((doc) => Promise.all([doc, ghUserEvents(user, doc.events_headers && doc.events_headers.etag)]))
       .then((p) => {
         let docs = []
@@ -58,7 +47,15 @@ db.list({include_docs: true, startkey: 'github:user:id:', endkey: 'github:user:i
           )
         )
       })
-      .then((x) => console.log(new Date(), user, x))
-      .catch((e) => console.log(new Date(), 'ERR:', user, e))
+      .then((x) => console.log(new Date(), 'EVENTS:  ', --count, user, x))
+      .catch((e) => {
+        --count
+        if (e.statusCode === 304) {
+          console.log(new Date(), 'NO-MODIF:', count, user, e.path)
+        } else {
+          console.log(new Date(), 'ERR:     ', count, user, e)
+          count = 0 // only set count to 0 on rate limit error
+        }
+      })
   })
 })
